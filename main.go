24c36821de1892998d7261e39cc70b2d034cbbc1
Brandon Kurtz <kurtz.brandon@gmail.com>
2021-12-30 20:53:10 -0800

    add fancy AzureBlobDownloader struct

--- main.go
+++ main.go
@@ -2,63 +2,105 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
+	"os"
+	"strings"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
 )
 
-var (
-	deviceCode *bool
-)
-
-func init() {
-	deviceCode = flag.Bool("device_code", false, "Determines if credential for Azure Blob operations should be device_code")
-	flag.Parse()
+type AzureBlobDownloader struct {
+	ClientID        string
+	TenantID        string
+	ContainerName   string
+	containerClient *azblob.ContainerClient
+	ctx             context.Context
 }
 
-func main() {
-	var credential azcore.TokenCredential
+func (c *AzureBlobDownloader) InitCredAndContainerClient() (*azblob.ContainerClient, error) {
 	// https://github.com/Azure/azure-sdk-for-go/blob/main/sdk/azidentity/device_code_credential.go
-	var err error
-	if *deviceCode {
-		credential, err = azidentity.NewDeviceCodeCredential(&azidentity.DeviceCodeCredentialOptions{
-			TenantID: tenantID,
-			ClientID: clientID,
-		})
-	} else {
-		credential, err = azidentity.NewInteractiveBrowserCredential(&azidentity.InteractiveBrowserCredentialOptions{
-			TenantID:    tenantID,
-			ClientID:    clientID,
-			RedirectURL: "http://localhost:9090",
-		})
-	}
+
+	credential, err := azidentity.NewDeviceCodeCredential(&azidentity.DeviceCodeCredentialOptions{
+		TenantID: tenantID,
+		ClientID: clientID,
+		UserPrompt: func(ctx context.Context, deviceCodeMessage azidentity.DeviceCodeMessage) error {
+			msg := strings.Replace(deviceCodeMessage.Message, "https://microsoft.com/devicelogin", "https://aka.ms/devicelogin", 1)
+			fmt.Println(msg)
+			return nil
+		},
+	})
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	ctx := context.Background()
 	container, err := azblob.NewContainerClient(
 		fmt.Sprintf("https://%s.blob.core.windows.net/%s", storageAccount, containerName),
 		credential,
 		&azblob.ClientOptions{},
 	)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	blob := container.NewBlobClient("azureblobtest")
-	blobProps, err := blob.GetProperties(ctx, &azblob.GetBlobPropertiesOptions{})
+	return &container, nil
+}
+
+func (c *AzureBlobDownloader) init() error {
+	if c.containerClient == nil {
+		client, err := c.InitCredAndContainerClient()
+		if err != nil {
+			return err
+		}
+		c.containerClient = client
+	}
+	if c.ctx == nil {
+		c.ctx = context.Background()
+	}
+	return nil
+}
+
+func WriteToFile(content io.ReadCloser, destination string) error {
+	out, err := os.Create(destination)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	_, err = io.Copy(out, content)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *AzureBlobDownloader) Download(asset, destination string) error {
+	if err := c.init(); err != nil {
+		return err
+	}
+	blob := c.containerClient.NewBlobClient(asset)
+	resp, err := blob.Download(c.ctx, &azblob.DownloadBlobOptions{})
+	if err != nil {
+		return err
+	}
+	body := resp.Body(azblob.RetryReaderOptions{})
+	return WriteToFile(body, destination)
+}
+
+func main() {
+	az := AzureBlobDownloader{
+		ClientID:      clientID,
+		TenantID:      tenantID,
+		ContainerName: containerName,
+	}
+	err := az.Download("azureblobtest", "azureblobtest.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("got this far")
-	fmt.Println(*blobProps.ContentType)
-	// resp, err := blob.Download(ctx, &azblob.DownloadBlobOptions{})
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// b, _ := ioutil.ReadAll(resp.Body(azblob.RetryReaderOptions{}))
-	// fmt.Println(string(b))
+	b, err := ioutil.ReadFile("azureblobtest.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(b))
+
 }
